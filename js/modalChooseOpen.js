2edf48a9cc2d5f4cada0302f5f6b3cba910b1e31
Biryukov-Vlad <ksander227@gmail.com>
2019-05-21 14:30:06 +0400

    modal

---
+++ js/modalChooseOpen.js
@@ -0,0 +1,11 @@
+var openChosseTop = document.getElementById('btnChoose');
+var closeListTop = document.getElementById('detailed-list__top');
+var arrowTop= document.getElementById('arrowTop')
+
+openChosseTop.onclick = function () {
+    if (closeListTop.style.display = 'none'){
+        closeListTop.style.display = 'flex';
+    }
+    
+}
+
